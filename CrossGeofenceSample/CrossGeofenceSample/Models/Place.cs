668dc6efef8fa33cc3fd7c5fcc5560f18894ee96
Daniel Villalba <dvillalba@tiempodevelopment.com>
2016-12-08 12:36:19 -0600

    Adding Place model

---
+++ CrossGeofenceSample/CrossGeofenceSample/Models/Place.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace CrossGeofenceSample.Models
+{
+    public class Place
+    {
+        public Place()
+        {
+        }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Loongitude { get; set; }
+        public double Radius { get; set; }
+    }
+}
