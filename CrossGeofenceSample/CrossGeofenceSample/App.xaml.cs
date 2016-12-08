6c56f94de7fc05e69274d348a3dae1e04c8ed156
Daniel Villalba <dvillalba@tiempodevelopment.com>
2016-12-08 13:18:24 -0600

    Adding PropertyChanged nuget; Adding ViewModels folder; Adding EventsViewModel at ViewModel folder

--- CrossGeofenceSample/CrossGeofenceSample/App.xaml.cs
+++ CrossGeofenceSample/CrossGeofenceSample/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace CrossGeofenceSample
 {
     public partial class App : Application
     {
+        public static ObservableCollection<Models.Event> Events = Helpers.Settings.GetEvents();
         public App()
         {
             InitializeComponent();
---
+++ CrossGeofenceSample/CrossGeofenceSample/ViewModels/EventsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using PropertyChanged;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using CrossGeofenceSample.Helpers;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CrossGeofenceSample.ViewModels
+{
+    [ImplementPropertyChanged]
+    public class EventsViewModel
+    {
+        public EventsViewModel()
+        {
+        }
+
+        public ObservableCollection<Models.Event> Data { get { return App.Events; } }
+    }
+}
