ed9a3dacb0c9c3fdbc1a2d98b04761cb42118c02
Daniel Villalba <dvillalba@tiempodevelopment.com>
2016-12-08 21:53:36 -0600

    Adding most of the view files, still need to complete the MapPage View

---
+++ CrossGeofenceSample/CrossGeofenceSample/Views/EventViewCell.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CrossGeofenceSample.Views
+{
+    public partial class EventViewCell : ViewCell
+    {
+        public EventViewCell()
+        {
+            InitializeComponent();
+        }
+
+        void OnDelete(object sender, EventArgs args)
+        {
+            var e = (Models.Event)this.BindingContext;
+
+            App.Events.Remove(e);
+            Helpers.Settings.RemoveEvent(e);
+        }
+    }
+}
---
+++ CrossGeofenceSample/CrossGeofenceSample/Views/MapPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CrossGeofenceSample.Views
+{
+    public partial class MapPage : ContentPage
+    {
+        bool isInitialized = false;
+        public static readonly BindableProperty LatitudeProperty = BindableProperty.Create<MapPage, double>(p => p.Latitude, 0.0);
+        public static readonly BindableProperty LongitudeProperty = BindableProperty.Create<MapPage, double>(p => p.Longitude, 0.0);
+
+        public double Latitude
+        {
+            get
+            {
+
+                return (double)GetValue(LatitudeProperty);
+            }
+            set
+            {
+                SetValue(LatitudeProperty, value);
+
+
+            }
+        }
+
+        public double Longitude
+        {
+            get
+            {
+                return (double)GetValue(LongitudeProperty);
+            }
+            set
+            {
+                SetValue(LongitudeProperty, value);
+            }
+        }
+
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
